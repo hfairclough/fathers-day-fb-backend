959549b563c5f24931a19c3f36e26284de6aefc9
Holly <holly.fairclough@gmail.com>
2025-06-15 08:37:59 +0100

    latest-commit

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const API_KEY = process.env.API_KEY; // Replace with your actual key
+//const API_KEY = process.env.API_KEY; // Replace with your actual key
+const API_KEY = 'bc987abf57msh476694d3ab2a69ap19e8ffjsn8b8a1a010625'
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Backend is working!');
-});
 
 // Route to get Liverpool matches
 app.get('/liverpool-fixtures', async (req, res) => {
   try {
-    const response = await fetch('https://api.football-data.org/v4/teams/64/matches?limit=100', {
-      headers: { 'X-Auth-Token': API_KEY }
+    const response = await fetch("https://api-football-v1.p.rapidapi.com/v3/fixtures?team=40&season=2024", {
+      method: "GET",
+      headers: { 
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com" 
+      }
     });
     const data = await response.json();
     res.json(data);
